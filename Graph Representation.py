561f9698ddae4cf2039216ffcc8f8db953fae3a9
mrechcheo149162536 <mrechcheo149162536@gmail.com>
2018-02-04 18:30:56 +0700

    Two way represent Graph

---
+++ Graph Representation.py
@@ -0,0 +1,20 @@
+def adjacencyMatrix(nodes, edges):
+    A = [[0]*nodes]*nodes
+
+    for i in range(edges):
+        print("Enter edge {0}: ".format(i+1))
+        x,y = map(int, input().split())
+        A[x-1][y-1] = 1
+    return A
+
+
+def adjacencyList(nodes, edges):
+    A = {};
+    for node in nodes:
+        A[node] = []
+    for i in range(edges):
+        print("Enter edge {0}: ".format(i+1))
+        x,y = input().split()
+        A[x].append(y)
+    return A
+
